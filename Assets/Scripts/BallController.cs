8154a1a35dc021b55a1faaa98029df07ce7586f5
Jeffrey Johnson <jefjohms@gmail.com>
2018-02-28 15:56:32 -0800

    have gameplay working.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -16,12 +16,22 @@ public class BallController : MonoBehaviour
         // start the ball rolling
         Vector2 direction = new Vector2(1.0f,-1.0f);
 	    _velocity = Speed * direction;
+	    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+	    if(rb != null)
+	    {
+            rb.AddForce(_velocity, ForceMode2D.Impulse);
+	        //rb.velocity = _velocity;
+	    }
+
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    this.transform.Translate(_velocity * Time.deltaTime);
+	    //this.transform.Translate(_velocity * Time.deltaTime);
+	    //Vector3 target = transform.position + new Vector3(_velocity.x, _velocity.y, 0);
+	    //this.transform.position = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
 	}
 
     void OnTriggerEnter2D(Collider2D other)
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    public float Speed;
+    public float MaxY;
+    public float MinY;
+
+    public bool IsLeftPaddle;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float inputValue = IsLeftPaddle ? Input.GetAxis("L_Paddle") : Input.GetAxis("R_Paddle");
+        
+	    Vector3 direction = -Vector3.down * inputValue * Speed * Time.deltaTime;
+
+        // clamp the y value to the max so paddles stop at the border.
+	    Vector3 newPosition = transform.position + direction;
+        float newYPosition = Mathf.Clamp(newPosition.y, MinY, MaxY);
+	    newPosition.y = newYPosition;
+	    transform.position = newPosition;
+	}
+}
